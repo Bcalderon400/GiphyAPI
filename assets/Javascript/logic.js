a793a2cc36cf82717f645e469e334b8efea5df47
Bcalderon400 <briancalderon200@gmail.com>
2019-07-20 17:14:49 -0700

    tried doing the pause gif thing still not working, but now the gifs clear after every button click

--- assets/Javascript/logic.js
+++ assets/Javascript/logic.js
@@ -5,11 +5,65 @@ var topics = [
 
 console.log(topics)
 
+function displayGifs() {
+    $('#gifs').empty()
+    var show = $(this).attr('data-name')
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + show + "&api_key=VXqJz0wVYIVmRpwkI5yT0uRFnpT5O93o&limit=5"
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response)
+
+        var results = response.data;
+        console.log(results)
+
+        for (var i = 0; i < results.length; i++) {
+            if (results[i].rating == 'r' || results[i].rating == 'pg-13') {
+
+            } else {
+                var showDiv = $('<div>')
+
+                var rate = $('<p>').text("Rating:" + results[i].rating)
+
+                var showImage = $('<img>')
+
+                showImage.attr('src', results[i].images.fixed_height.url)
+                showImage.attr('data-still', results[i].images.fixed_height_still.url)
+                showImage.attr('data-animate', results[i].images.fixed_height.url)
+                showImage.attr('data-state', 'still')
+                showImage.addClass('gif')
+
+                showDiv.append(rate)
+                showDiv.append(showImage)
+
+                $('#gifs').prepend(showDiv)
+            }
+
+        }
+
+    })
+
+}
+
+$('.gif').on('click', function() {
+    var state = $(this).attr('data-state')
+
+    if (state === 'still') {
+        $(this).attr('src', $(this).data('animate'));
+        $(this).attr('data-state', 'animate');
+    } else {
+        $(this).attr('src', $(this).data('still'));
+        $(this).attr('data-state', 'still');
+    }
+})
+
 function renderButtons() {
-    $('#cartoon-buttons').empty()
+
+    $('#cartoon-buttons').empty();
 
     for (var i = 0; i < topics.length; i++) {
-
         var b = $('<button>')
 
         b.addClass('cartoon')
@@ -17,46 +71,12 @@ function renderButtons() {
         b.text(topics[i])
 
         $('#cartoon-buttons').append(b)
+
+        console.log(b)
     }
-    console.log(b)
-
-
-    $('button').on('click', function() {
-
-        var show = $(this).attr('data-name')
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + show + "&api_key=VXqJz0wVYIVmRpwkI5yT0uRFnpT5O93o&limit=05"
-
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function(response) {
-            console.log(response)
-
-            var results = response.data;
-            console.log(results)
-            for (var i = 0; i < results.length; i++) {
-                var showDiv = $('<div>')
-
-                var rate = $('<p>').text("Rating:" + results[i].rating)
-
-                var showImage = $('<img>')
-
-                showImage.attr('src', results[i].images.preview_gif.url)
-                showImage.attr('gif')
-                showDiv.append(rate)
-                showDiv.append(showImage)
-
-                $('#gifs').prepend(showDiv)
-
-
-
-            }
-        })
-    })
-
 }
 
-$(document).on("click", '#add-cartoon', function(event) {
+$("#add-cartoon").on("click", function(event) {
 
     event.preventDefault()
 
@@ -67,5 +87,9 @@ $(document).on("click", '#add-cartoon', function(event) {
 
     renderButtons()
 
+
+
 });
+
+$(document).on('click', '.cartoon', displayGifs)
 renderButtons()
